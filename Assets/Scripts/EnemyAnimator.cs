fcee315a7e89585b514ebbae9ab31d7a3806e24b
makiset <mlh25080@gmail.com>
2025-03-15 17:26:30 -0400

    potentially functional enemy movement animator

--- Assets/Scripts/EnemyAnimator.cs
+++ Assets/Scripts/EnemyAnimator.cs
@@ -1,9 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static EnemyMovement;
 
 public class EnemyAnimator : MonoBehaviour
 {
+    [SerializeField] Transform mainTransform;
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] EnemyMovement em;
 
-    
+    private void Update()
+    {
+        if (em.enemyState == EnemyState.PATROL)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else if (em.enemyState == EnemyState.CHASE)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else if (em.enemyState == EnemyState.ATTACK)
+        {
+            ;
+        }
+        else if (em.enemyState == EnemyState.IDLE)
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+
 }
